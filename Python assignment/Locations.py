8b5b70aacc0dc1953fe486775228f0e57a9a70ef
princelymax <138497534+princelymax@users.noreply.github.com>
2025-11-19 22:50:15 +0100

    Add files via upload

---
+++ Python assignment/Locations.py
@@ -0,0 +1,23 @@
+Locations = ['Paris','New zealand','USA','Spain','Italy']
+print(Locations)
+Locations.sort()
+print(Locations)
+
+Locations = ['Paris','New zealand','USA','Spain','Italy']
+print(Locations)
+Locations.sort(reverse=True)
+print(Locations)
+
+Locations = ['Paris','New zealand','USA','Spain','Italy']
+print(Locations)
+Locations.reverse()
+print(Locations)
+
+Locations.reverse()
+print(Locations)
+
+Locations.sort()
+print(Locations)
+
+Locations.sort(reverse=True)
+print(Locations)
---
+++ Python assignment/guest_list.py
@@ -0,0 +1,25 @@
+#Ex 3.4
+Guest = ['Favour','Maira','Max','Ngwa']
+print(Guest)
+print('Hello',Guest[0],'i will like to invite you to diner at my place')
+print('Hello',Guest[1],'i will like to invite you to diner at my place')
+print('Hello',Guest[2],'i will like to invite you to diner at my place')
+print('Hello',Guest[3],'i will like to invite you to diner at my place')
+#3.5
+print('for some reasons',Guest[3],'cant make it ti diner')
+Guest[3]= 'Nji'
+print(Guest)
+#3.6
+print('Hey guys, i just found a bigger table for diner')
+Guest.insert(0,'Bless')
+Guest.insert(2,'Faith')
+Guest.insert(4,'Cindy')
+print(Guest)
+
+print('Hello',Guest[0],'i will like to invite you to diner at my place')
+print('Hello',Guest[1],'i will like to invite you to diner at my place')
+print('Hello',Guest[2],'i will like to invite you to diner at my place')
+print('Hello',Guest[3],'i will like to invite you to diner at my place')
+print('Hello',Guest[4],'i will like to invite you to diner at my place')
+print('Hello',Guest[5],'i will like to invite you to diner at my place')
+print('Hello',Guest[6],'i will like to invite you to diner at my place')
